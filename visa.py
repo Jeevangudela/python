7c45d7e1e67f06e53a61d63272225a2dcf9c1adb
Jeevangudela <emp4@widesigninc.com>
2023-07-20 12:16:18 +0530

    total code updated

--- visa.py
+++ visa.py
@@ -1,13 +1,123 @@
-set1_countries = {'USA', 'Indonesia', 'Australia', 'Germany','Netherlands','Italy','Spain','France','United Kingdom'}
+set1_countries = {'USA':450, 'Indonesia':765, 'Australia':875, 'Germany':643,'Netherlands':536,'Italy':382,'Spain':835,'United Kingdom':868}
+
+
+
+
 set2_countries = {'Brazil', 'France', 'India', 'Japan'}
 
-passport_name = input("Enter a country name of the passport holder: ")
-destination_name = input("Enter a destination country name :")
-country_name = input("Enter a Trasnit country name :")
 
-if country_name in set1_countries:
-    print("Transit Visa Required")
-elif country_name in set2_countries:
-    print("Transit visa Not Requried")
+
+
+transit_countries = int(input("Enter your transit countries: "))
+
+
+
+
+if transit_countries == 1:
+
+   
+
+   
+
+    passport_name = input("Enter a country name of the passport holder: ")
+
+   
+
+    destination_name = input("Enter a destination country name :")
+
+   
+
+    country_name = input("Enter a Trasnit country name :")
+
+   
+
+    if country_name in set1_countries:
+
+       
+
+        print("Transit Visa Required")
+
+       
+
+    elif country_name in set2_countries:
+
+       
+
+        print("Transit visa Not Requried")
+
+       
+
+    else:
+
+       
+
+        print("Country not found in either set")
+
+
+
+
 else:
-    print("Country not found in either set")
+
+   
+
+    transit_country1, transit_country2 = input("Enter two transit countries: ").split()
+
+   
+
+    print("First transit country is {} and second transit country is {}".format(transit_country1, transit_country2))
+
+ 
+
+
+
+
+    passport_name = input("Enter a country name of the passport holder: ")
+
+   
+
+    destination_name = input("Enter a destination country name :")
+
+   
+
+   
+
+    if transit_country1 in set1_countries:
+
+       
+
+        print("Transit Visa Required for ", transit_country1)
+
+       
+
+    if transit_country1 in set2_countries:
+
+           
+
+         print("Transit Visa not Required for", transit_country1)
+
+
+
+
+   
+
+    if transit_country2 in set1_countries:
+
+       
+
+        print("Transit Visa Required for ", transit_country2)
+
+       
+
+    if transit_country2 in set2_countries:
+
+           
+
+         print("Transit Visa not Required for", transit_country2)
+
+         
+
+    else:
+
+       
+
+        print("Country not found in either set")
